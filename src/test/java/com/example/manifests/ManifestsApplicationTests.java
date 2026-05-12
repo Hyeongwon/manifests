dbba48f1adc1362be72c4b28f1541e5aba498e6f
hyeongwon byun <hyeongwonbyun@hyeongwonui-MacBookPro.local>
2026-05-12 22:05:49 +0900

    .

---
+++ src/test/java/com/example/manifests/ManifestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manifests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManifestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
